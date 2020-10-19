744687a1c9a87537e0df3958c724b1b736587242
Joshua Kevin <jokevin.racmadi@gmail.com>
2020-10-19 13:26:38 +0700

    new commit isi laravel

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+@extends('adminlte::page')
+
+@section('title', 'Panti Asuhan Anugrah Oke')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+
+    <h3>Selamat datang di panti anugrah</h3>
+
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
