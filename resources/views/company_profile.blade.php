2e26665f2f0a5aeac2a38e6c27545b3102cc538c
Joshua Kevin <jokevin.racmadi@gmail.com>
2020-12-04 14:45:31 +0700

    penambahan home page

---
+++ resources/views/company_profile.blade.php
@@ -0,0 +1,123 @@
+<!doctype html>
+<!--[if IE 7 ]>    <html lang="en-gb" class="isie ie7 oldie no-js"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en-gb" class="isie ie8 oldie no-js"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en-gb" class="isie ie9 no-js"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!-->
+<html lang="en-gb" class="no-js">
+<!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <!--[if lt IE 9]> 
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <![endif]-->
+    <title>RPA ANUGERAH BOYOLALI</title>
+    <meta name="description" content="">
+    <meta name="author" content="WebThemez">
+    <!--[if lt IE 9]>
+        <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <!--[if lte IE 8]>
+		<script type="text/javascript" src="http://explorercanvas.googlecode.com/svn/trunk/excanvas.js"></script>
+	<![endif]-->
+    {{-- <link rel="stylesheet" href="css/bootstrap.min.css" /> --}}
+    <link rel="stylesheet" type="text/css" href="css/isotope.css" media="screen" />
+    <link rel="stylesheet" href="js/fancybox/jquery.fancybox.css" type="text/css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="css/da-slider.css" />
+    <!-- Owl Carousel Assets -->
+    <link href="js/owl-carousel/owl.carousel.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles.css" />
+    <!-- Font Awesome -->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+    {{-- <link rel="stylesheet" href="{{ asset('vendor/adminlte/dist/css/adminlte.min.css') }}"> --}}
+    {{-- <link rel="stylesheet" href="bootstrap/css/bootstrap.css"> --}}
+    {{-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> --}}
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+
+<body>
+    <header class="header">
+        <div class="row">
+            <div class="col-12">
+                
+                <nav class="navbar navbar-dark bg-dark navbar-expand-lg" role="navigation" >
+                    <div class="navbar-header ml-2">
+                       
+                         <!--/.navbar-header-->
+                        <a href="#" class="navbar-brand scroll-top logo "><b>RUMAH PEDULI ANAK ANUGERAH</b></a>
+                    </div>
+                    
+                  
+                    <div class="collapse navbar-collapse">
+                        
+                    </div>
+                    <div class="form-inline">
+                        
+                        <a href="{{route('home')}}" class="btn btn-primary">Login <i class="fa fa-power-off"></i></a>
+                    </div>
+                    <!--/.navbar-collapse-->
+                </nav>
+            </div>
+        </div>
+       
+        <!--/.container-->
+    </header>
+    <div class="container">
+        <section id="home" class="mt-2">
+            <div class="banner-container" style="text-align: center">
+                <img src="images/nn.jpg" alt="banner" />
+                <div class="container banner-content">
+                    <div id="da-slider" class="da-slider">
+                            <div class="da-img"></div>
+                       
+    
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="introText">
+            <div class="container">
+                <div class="text-center">
+                
+                  <p></p>
+                </div>
+            </div>
+    
+    
+        </section>
+        <center><h1>SEJARAH:<span class=<h1> </B><center>
+					
+            <h2>Panti Asuhan Anugerah ini berdiri sejak tahun 2015</p> 
+            <h2>beralamat di Desa Ngaduman RT08/RW02 Kecamatan Mojosongo,Kabupaten Boyolali, Jawa Tengah</p>
+            <h2>yang mendasari berdirinya Panti Asuhan ini adalah karena kecintaan dan kepedulian pendiri terhadap kehidupan anak-anak yang terlantar.</p>
+                    <h2>hal ini yang menjadi awal dari berdiri nya Panti Asuhan Ini</p>
+                <h2>juga karena pengalaman hidupnya, yang membuat Pendiri ingin agar anak-anak asuh dapat kehidupan yang layak</p>
+                serta  membantu mereka mencapai cita-cita mereka.
+                
+            </p>
+        
+		<center><h1>VISI-MISI:<Span Class=</h1></B><center>
+                
+            <h2>Menjadi sarana dan tempat pembinaan bagi anak-anak</p>
+            <h2>dan masyarakat yang memiliki latar belakang kurang mampu,yatim, piatu dan juga yatim piatu</P>
+
+                <center><h1>TUJUAN:<span class=</h1></B><center>
+                    <h2>Membentuk Dan Menumbuh Kembangkan Iman Dan 
+                    Ketakwaan Terhadap Tuhan Yang Maha Esa</P>
+                    <h2>Serta Berperan Secara Aktif Dalam Meningkatkan Kesejahteraan Masyarakat.</p>
+                    <h2>Terutama Bagi Mereka Anak-Anak Penyandang Masalah Kesejahteraan Sosial,</p>
+                    <h2>Sehingga Mereka Mendapatkan Kehidupan Yang Layak.</p>
+            </div>
+    </div>
+
+</body>
+
+
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
--- resources/views/vendor/adminlte/auth/auth-page.blade.php
+++ resources/views/vendor/adminlte/auth/auth-page.blade.php
@@ -19,12 +19,12 @@
     <div class="{{ $auth_type ?? 'login' }}-box">
 
         {{-- Logo --}}
-        <div class="{{ $auth_type ?? 'login' }}-logo">
+        {{-- <div class="{{ $auth_type ?? 'login' }}-logo">
             <a href="{{ $dashboard_url }}">
                 <img src="{{ asset(config('adminlte.logo_img')) }}" height="50">
                 {!! config('adminlte.logo', '<b>Admin</b>LTE') !!}
             </a>
-        </div>
+        </div> --}}
 
         {{-- Card Box --}}
         <div class="card {{ config('adminlte.classes_auth_card', 'card-outline card-primary') }}">
